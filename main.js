c2b9234aa1ac41de78e1a21992bef1132ec2555f
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-05-27 18:44:36 +0200

    chuck api

---
+++ main.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+  var baseUrl = 'https://api.chucknorris.io/jokes/';
+
+  $.ajax({
+    url: baseUrl + 'categories',
+    method: 'GET',
+    success: function(data){
+      for (var i = 0; i < data.length; i++) {
+        var category = data[i];
+        $('.category').append('<option value="' + category + '">' + category +'</option>');
+      }
+    }, error: function (error) {
+        alert(error);
+    }
+  });
+
+
+  $('#bottone-search').click(function () {
+    var categoriaSelezionata = $('.category').val();
+    $.ajax({
+      url: baseUrl + 'random',
+      method: 'GET',
+      data: {
+        category: categoriaSelezionata
+      },
+      success: function (data) {
+        makeJoke(data);
+      }
+    })
+  });
+
+  $('#text-input').keyup(function (e) {
+
+      if(e.which == 13){
+        var input = $('#text-input').val();
+        $.ajax({
+          url: baseUrl + 'search',
+          method: 'GET',
+          data: {
+            query: input
+          },
+          success: function (data) {
+            console.log(data);
+            if(data.total > 0){
+              var random = Math.floor(Math.random() * data.total);
+              var joke = data.result[random];
+              makeJoke(joke);
+            }
+          }
+
+        })
+      }
+  });
+
+  // funzione per creare jokes
+  function makeJoke(data) {
+    var source = $("#category-template").html();
+    var template = Handlebars.compile(source);
+    var context = {
+      img: data.icon_url,
+      joke: data.value,
+      data: data.updated_at
+    };
+    var html = template(context);
+
+    $('.contenitore-joke').append(html);
+  }
+
+
+
+})
